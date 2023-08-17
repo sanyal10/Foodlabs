3ec93eebaff0088896e3b238593e44f21a385f5e
sanyal10 <sanyaldebajyoti2003@gmail.com>
2023-08-17 21:39:57 +0530

    Updates

---
+++ precache-manifest.e67c9ac3ae4c23d50cd0dd3cc3283aee.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "55e6c51f8335a7200808",
+    "url": "/Foodlabs/static/css/main.386d4d4b.chunk.css"
+  },
+  {
+    "revision": "55e6c51f8335a7200808",
+    "url": "/Foodlabs/static/js/main.55e6c51f.chunk.js"
+  },
+  {
+    "revision": "0f42ad21f20e3536abc0",
+    "url": "/Foodlabs/static/js/1.0f42ad21.chunk.js"
+  },
+  {
+    "revision": "a5ae8228d8d3814285ca",
+    "url": "/Foodlabs/static/css/2.700da931.chunk.css"
+  },
+  {
+    "revision": "a5ae8228d8d3814285ca",
+    "url": "/Foodlabs/static/js/2.a5ae8228.chunk.js"
+  },
+  {
+    "revision": "06b098547330481e8d6d",
+    "url": "/Foodlabs/static/js/runtime~main.06b09854.js"
+  },
+  {
+    "revision": "73a9ed5e72d43723c4106f6714861ae8",
+    "url": "/Foodlabs/static/media/fdlbs.73a9ed5e.png"
+  },
+  {
+    "revision": "cde1a1d09b536cc2b27d80ade0e2b2b5",
+    "url": "/Foodlabs/static/media/bodybg.cde1a1d0.png"
+  },
+  {
+    "revision": "de08ba64b45ead4227d1a1da64c28b4d",
+    "url": "/Foodlabs/static/media/img3.de08ba64.png"
+  },
+  {
+    "revision": "77fa3578dc623157cdd86250ca3e91d9",
+    "url": "/Foodlabs/static/media/img4.77fa3578.png"
+  },
+  {
+    "revision": "ccfde405eea7a59942976ad3df1e9fb5",
+    "url": "/Foodlabs/static/media/img5.ccfde405.png"
+  },
+  {
+    "revision": "63d26e406bc2f3881c7c52f38e56b747",
+    "url": "/Foodlabs/static/media/img1.63d26e40.png"
+  },
+  {
+    "revision": "fc7fc30d4bb9fefef328c755b10bb64e",
+    "url": "/Foodlabs/static/media/img2.fc7fc30d.png"
+  },
+  {
+    "revision": "69005bc2df59d41ba16cf4b4aa47c6e9",
+    "url": "/Foodlabs/static/media/img3.69005bc2.png"
+  },
+  {
+    "revision": "6792f362d9b381db89f3013fbb6becb8",
+    "url": "/Foodlabs/static/media/img4.6792f362.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Foodlabs/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "42290c228423d2ba3ed21cceae91ab69",
+    "url": "/Foodlabs/index.html"
+  }
+];
